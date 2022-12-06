c78203b7b191bf9fa6a71c5c8a0df5e0d51ca328
Blancacmps <blancadimasc@gmail.com>
2022-12-06 13:31:13 -0300

    versão-01

---
+++ priemeiromodulo.js
@@ -0,0 +1,4 @@
+exports.nome = function() {
+    nome = 'Blanca';
+    return nome;
+}
---
+++ servidorweb.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var n = require('./primeiromodulo');
+var dia = require('./moduloData');
+
+
+http.createServer(function (req, res){
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write('Aula de Node.js <br>');
+    res.write('Aluna: ');
+    res.write(n.nome()+ '<br>');
+
+    res.write('Data: ');
+    res.write(dia.diaDaSemana()+ '<br>');
+
+ 
+    res.end('<br> Paz!');
+       }).listen(80);
